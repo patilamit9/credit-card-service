517bb10d56ef153fbcc78d124f07e1c59072f0e2
Amit Patil <amitpatil@PUN-IT000541.local>
2020-03-19 03:56:02 +0530

    backend service in kotlin and springboot 2.2

---
+++ backend-springboot-kotlin-coroutines/credit-card-service/src/test/kotlin/com/creditcard/ms/creditcardservice/CreditCardServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.creditcard.ms.creditcardservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditCardServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
